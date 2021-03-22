a8aeb80e50023ed9d05f5ceada86d4c7f3257c38
RaghuVardhan42 <80853507+RaghuVardhan42@users.noreply.github.com>
2021-03-22 13:23:32 +0530

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+
+st.text_input("username")
+
+uploaded_file = st.file_uploader("Upload Files",type=['csv'])
+
+butt=st.button("submit")
+if (butt==True):
+    df=pd.read_csv(uploaded_file)
+    st.write(df.head())
+
+    with st.echo(code_location='below'):
+        fig = px.scatter(x=df["sepal.length"],y=df["petal.length"],)
+        fig.update_layout(xaxis_title="sepal length",yaxis_title="petal length",)
+
+        st.write(fig)
